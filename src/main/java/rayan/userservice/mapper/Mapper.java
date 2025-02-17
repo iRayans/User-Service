6bdf075b0e8aea09bd3e4d09545140f63af789f0
rharbi <iirharbi@gmail.com>
2025-02-17 22:08:12 +0300

    Mapper to map DTOs to User object & UserReadOnlyDTO

---
+++ src/main/java/rayan/userservice/mapper/Mapper.java
@@ -0,0 +1,34 @@
+package rayan.userservice.mapper;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import rayan.userservice.core.enums.RoleType;
+import rayan.userservice.dto.UserInsertDTO;
+import rayan.userservice.dto.UserReadOnlyDTO;
+import rayan.userservice.entity.User;
+import rayan.userservice.security.PasswordUtil;
+
+import java.time.LocalDateTime;
+
+@ApplicationScoped
+public class Mapper {
+
+
+    public User mapToUser(UserInsertDTO userInsertDTO) {
+        return new User(null,
+                userInsertDTO.getUsername(),
+                userInsertDTO.getEmail(),
+                PasswordUtil.encryptPassword(userInsertDTO.getPassword()),
+                RoleType.valueOf(userInsertDTO.getRole()),
+                LocalDateTime.now(),
+                LocalDateTime.now());
+    }
+
+    public UserReadOnlyDTO mapToUserReadOnlyDTO(User user) {
+        return new UserReadOnlyDTO(user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getRole().name(),
+                user.getCreatedAt(),
+                user.getUpdatedAt());
+    }
+}
