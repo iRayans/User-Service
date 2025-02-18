5cd8c8c7b35aec7a7ffc01ffb1636712aa7377ff
rharbi <iirharbi@gmail.com>
2025-02-18 14:47:10 +0300

    Implement AuthenticationResource for user authentication process

---
+++ src/main/java/rayan/userservice/rest/AuthenticationResource.java
@@ -0,0 +1,39 @@
+package rayan.userservice.rest;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import rayan.userservice.core.excpetion.AppServerException;
+import rayan.userservice.core.excpetion.EntityAlreadyExistsException;
+import rayan.userservice.core.excpetion.EntityInvalidArgumentsException;
+import rayan.userservice.core.util.ValidatorUtil;
+import rayan.userservice.dto.user.UserInsertDTO;
+import rayan.userservice.dto.user.UserReadOnlyDTO;
+import rayan.userservice.service.UserService;
+
+@Path("/auth")
+public class AuthenticationResource {
+
+    @Inject
+    UserService userService;
+    @Inject
+    ValidatorUtil validatorUtil;
+    @POST
+    @Path("/register")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response registerUser(UserInsertDTO userInsertDTO)
+            throws AppServerException, EntityInvalidArgumentsException, EntityAlreadyExistsException {
+        // Validation
+        validatorUtil.validateDTO(userInsertDTO);
+
+        UserReadOnlyDTO userReadOnlyDTO = userService.createUser(userInsertDTO);
+        return Response.status(Response.Status.CREATED)
+                .entity(userReadOnlyDTO)
+                .build();
+    }
+}
--- src/main/java/rayan/userservice/rest/UserResource.java
+++ src/main/java/rayan/userservice/rest/UserResource.java
@@ -1,54 +1,6 @@
 package rayan.userservice.rest;
 
-import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
-import rayan.userservice.dao.UserDAO;
-import rayan.userservice.dto.UserInsertDTO;
-import rayan.userservice.dto.UserReadOnlyDTO;
-import rayan.userservice.entity.User;
-import rayan.userservice.service.UserService;
-
-import java.util.List;
-import java.util.Optional;
-
-@Path("users")
-@Produces(MediaType.APPLICATION_JSON)
-@Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
 
-    @Inject
-    UserService userService;
-
-
-    @POST
-    @Transactional
-    public UserReadOnlyDTO createUser(UserInsertDTO userInsertDTO) {
-        return userService.createUser(userInsertDTO);
-    }
-
-//    @GET
-//    public List<User> hello() {
-//        return userDAO.findAll();
-//    }
-//
-//    @GET
-//    @Path("/{id}")
-//    public Optional<User> getUser(@PathParam("id") Long id) {
-//        return userDAO.findById(id);
-//    }
-//
-//    @PUT
-//    @Path("/{id}")
-//    public User updateUser(@PathParam("id") Long id, User user) {
-//        return userDAO.update(user);
-//    }
-//
-//    @DELETE
-//    @Path("/{id}")
-//    public String deleteUser(@PathParam("id") Long id) {
-//        userDAO.delete(id);
-//        return "User with id " + id + " deleted";
-//    }
+// TODO:Handle user operation.
 }
