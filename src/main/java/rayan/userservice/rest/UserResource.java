8008d16b73aa17a9ea5f66acdd04e82d4ea96398
rharbi <iirharbi@gmail.com>
2025-02-20 12:36:28 +0300

    Implement UserResource

--- src/main/java/rayan/userservice/rest/UserResource.java
+++ src/main/java/rayan/userservice/rest/UserResource.java
@@ -1,6 +1,30 @@
 package rayan.userservice.rest;
 
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import rayan.userservice.dto.user.UserReadOnlyDTO;
+import rayan.userservice.service.UserService;
+import rayan.userservice.service.UserServiceImp;
+
+import java.util.List;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
-// TODO:Handle user operation.
+    @Inject
+    UserService userService;
+
+
+    @GET
+    public Response getAllUsers() {
+        List<UserReadOnlyDTO> users = userService.getAllUsers();
+        return Response.ok(users).entity(users).build();
+    }
 }
