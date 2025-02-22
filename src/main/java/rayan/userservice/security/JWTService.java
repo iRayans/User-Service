fd30232a79af28b8548a079721a9aeb611cc3795
rharbi <iirharbi@gmail.com>
2025-02-22 23:42:49 +0300

    Implement JWT token generation and configuration

---
+++ src/main/java/rayan/userservice/security/JWTService.java
@@ -0,0 +1,93 @@
+package rayan.userservice.security;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.function.Function;
+
+import javax.crypto.SecretKey;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.enterprise.context.ApplicationScoped;
+import rayan.userservice.core.util.ConfigFileUtil;
+import rayan.userservice.entity.User;
+
+@ApplicationScoped
+public class JWTService {
+
+    // In a real project, this should be loaded from environment variables
+    private static String SECRET_KEY = "";
+    private static long JWT_EXPIRATION = 0L;
+
+    static {
+        Properties configProperties = ConfigFileUtil.getPropertiesInstance();
+        SECRET_KEY = configProperties.getProperty("jwt.secretKey");
+        JWT_EXPIRATION = Long.valueOf(configProperties.getProperty("jwt.expiration"));
+    }
+
+    public String generateToken(String email, String name, String role) {
+        var claims = new HashMap<String, Object>();
+        claims.put("username", name);
+        claims.put("role", role);
+        return Jwts
+                .builder()
+                .setIssuer("st")
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        final String subject = extractSubject(token);
+        return (subject.equals(user.getEmail())) && !isTokenExpired(token);
+    }
+
+    // Here we mean extract username
+    public String extractSubject(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    /**
+     * Creates a HS256 Key. Key is an interface.
+     * Starting from secretKey we get a byte array
+     * of the secret. Then we get the {@link SecretKey},
+     * class that implements the {@link Key } interface.
+     *
+     * @return a SecretKey which implements Key.
+     */
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
